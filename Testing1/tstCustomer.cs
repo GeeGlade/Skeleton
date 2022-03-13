cc951bea9b3539d3fdc1df06c7a2c64a2f5f8567
Adam <P2526374@my365.dmu.ac.uk>
2022-03-13 15:38:34 +0000

    Added properties to clsCustomer class and tested everything in the class

--- Testing1/tstCustomer.cs
+++ Testing1/tstCustomer.cs
@@ -15,5 +15,82 @@ namespace Testing1
             //Test to see if it exists
             Assert.IsNotNull(TestCustomer);
         }
+
+        [TestMethod]
+        public void CustomerIDOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            Int32 testData = 1;
+            //Set property equal to test data
+            TestCustomer.CustomerID = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.CustomerID, testData);
+        }
+
+        [TestMethod]
+        public void CustomerUsernameOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            String testData = "TestName12345";
+            //Set property equal to test data
+            TestCustomer.CustomerUsername = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.CustomerUsername, testData);
+        }
+
+        [TestMethod]
+        public void CustomerPasswordOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            String testData = "TestPass12345";
+            //Set property equal to test data
+            TestCustomer.CustomerPassword = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.CustomerPassword, testData);
+        }
+
+        [TestMethod]
+        public void DateAddedOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            DateTime testData = DateTime.Now.Date;
+            //Set property equal to test data
+            TestCustomer.DateAdded = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.DateAdded, testData);
+        }
+
+        [TestMethod]
+        public void Over18PropertyOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            Boolean testData = true;
+            //Set property equal to test data
+            TestCustomer.Over18 = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.Over18, testData);
+        }
+
+        public void BillingShippingAddressOK()
+        {
+            //Create instance of clsCustomer class
+            clsCustomer TestCustomer = new clsCustomer();
+            //Test data generated
+            String testData = "fake street 123";
+            //Set property equal to test data
+            TestCustomer.BillingShippingAddress = testData;
+            //Test to see if it functions as expected
+            Assert.AreEqual(TestCustomer.BillingShippingAddress, testData);
+        }
     }
 }

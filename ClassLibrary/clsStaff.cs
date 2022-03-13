bd58e73474e37b9aa656d8ada0b12cefbacd2f1f
Safwan Waraich <P2627322@my365.dmu.ac.uk>
2022-03-13 20:50:49 +0000

    Created properties for the table design

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -2,5 +2,13 @@
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DOB { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public float Salary { get; set; }
     }
 }
--- Testing3/UnitTest1.cs
+++ Testing3/UnitTest1.cs
@@ -15,5 +15,101 @@ namespace Testing3
             Assert.IsNotNull(AnStaff);
 
         }
+
+        [TestMethod]
+        public void ActivePropertOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            Boolean TestData = true;
+
+            AnStaff.Active = TestData;
+
+            Assert.AreEqual(AnStaff.Active, TestData);
+        }
+
+        [TestMethod]
+        public void FirstNameOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "Bob";
+
+            AnStaff.FirstName = TestData;
+
+            Assert.AreEqual(AnStaff.FirstName, TestData);
+        }
+
+        [TestMethod]
+        public void LastNameOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "Smith";
+
+            AnStaff.LastName = TestData;
+
+            Assert.AreEqual(AnStaff.LastName, TestData);
+        }
+
+        [TestMethod]
+        public void DOBOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "23/04/2000";
+
+            AnStaff.DOB = TestData;
+
+            Assert.AreEqual(AnStaff.DOB, TestData);
+        }
+
+        [TestMethod]
+        public void UsernameOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "User123";
+
+            AnStaff.Username = TestData;
+
+            Assert.AreEqual(AnStaff.Username, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "Password234";
+
+            AnStaff.Password = TestData;
+
+            Assert.AreEqual(AnStaff.Password, TestData);
+        }
+
+        [TestMethod]
+        public void GenderOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            String TestData = "M";
+
+            AnStaff.Gender = TestData;
+
+            Assert.AreEqual(AnStaff.Gender, TestData);
+        }
+
+        [TestMethod]
+        public void SalaryOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+
+            float TestData = 20000;
+
+            AnStaff.Salary = TestData;
+
+            Assert.AreEqual(AnStaff.Salary, TestData);
+        }
     }
 }

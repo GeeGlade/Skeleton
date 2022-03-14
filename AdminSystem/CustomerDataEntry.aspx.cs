3ac275716563f712f1b8bcab83a8099c130199f3
Adam <P2526374@my365.dmu.ac.uk>
2022-03-14 11:19:12 +0000

    Viewing stuff for the page

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer.CustomerUsername = txtCustomerUsername.Text;
+        ACustomer.CustomerPassword = txtCustomerPassword.Text;
+        //ACustomer.DateAdded = txtDateAdded.Text;
+        ACustomer.BillingShippingAddress = txtBSA.Text;
+        ACustomer.Over18 = chkOver18.Checked;
+
+        Session["ACustomer"] = ACustomer;
+        //Navigate to viewer page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer = (clsCustomer)Session["ACustomer"];
+        Response.Write(ACustomer.CustomerID + "\n");
+        Response.Write(ACustomer.CustomerUsername + "\n");
+        Response.Write(ACustomer.CustomerPassword + "\n");
+        Response.Write(ACustomer.BillingShippingAddress + "\n");
+        Response.Write(ACustomer.Over18);
     }
 }

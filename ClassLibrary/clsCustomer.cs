26d024c5ed93147f16ea1a07e183fd9b87edeca3
Adam <P2526374@my365.dmu.ac.uk>
2022-05-15 21:59:31 +0100

    Adam U9

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -102,5 +102,65 @@ namespace ClassLibrary
             //If database is not found
             return false;
         }
+
+        public string Valid(string customerUsername, string customerPassword, string dateAdded, string billingShippingAddress)
+        {
+            string Error = "";
+
+
+            if(customerUsername.Length == 0)
+            {
+                Error += "Username must not be empty. ";
+            }
+
+            if (customerUsername.Length > 20)
+            {
+                Error += "Username must be at most 20 characters. ";
+            }
+
+            if (customerPassword.Length == 0)
+            {
+                Error += "Password must not be empty. ";
+            }
+
+            if (customerPassword.Length > 20)
+            {
+                Error += "Password must be at most 20 characters. ";
+            }
+
+            try
+            {
+                //copy the dateAdded value to the DateTemp variable
+                DateTime  DateTemp = Convert.ToDateTime(dateAdded);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    //record the error
+                    Error += "Date cannot be in the past. ";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error += "Date cannot be in the future. ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error += "Invalid date. ";
+            }
+
+            if (billingShippingAddress.Length == 0)
+            {
+                Error += "Billing Shipping Address must not be empty. ";
+            }
+
+            if (billingShippingAddress.Length > 50)
+            {
+                Error += "Billing Shipping Address must be at most 50 characters. ";
+            }
+
+            return Error;
+        }
     }
 }

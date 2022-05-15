702b8874ab450db276882c5be84fd52684d19cf2
Adam <P2526374@my365.dmu.ac.uk>
2022-05-15 19:13:00 +0100

    part 3

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -26,4 +26,27 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //Navigate to viewer page
         Response.Redirect("CustomerViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //Create instance of clsCustomer class
+        clsCustomer btnCustomer = new clsCustomer();
+        //Store primary key
+        Int32 CustomerID;
+        //Store result of find method
+        Boolean Found = false;
+        //Get primary key entered by user
+        CustomerID = Convert.ToInt32(txtCustomerID.Text);
+        //Find record
+        Found = btnCustomer.Find(CustomerID);
+        if(Found)
+        {
+            //Add data to form
+            txtCustomerUsername.Text = btnCustomer.CustomerUsername;
+            txtCustomerPassword.Text = btnCustomer.CustomerPassword;
+            txtDateAdded.Text = btnCustomer.DateAdded.ToString();
+            txtBSA.Text = btnCustomer.BillingShippingAddress;
+            chkOver18.Checked = btnCustomer.Over18;
+        }
+    }
 }

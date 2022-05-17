257cc5230f0c847f53074e02ae4da730ef8133eb
Samuel Heelis <P2532413@my365.dmu.ac.uk>
2022-05-17 01:08:25 +0100

    Added  some test

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -4,11 +4,99 @@ namespace ClassLibrary
 {
     public class clsOrders
     {
-        public bool Active { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string OrderContents { get; set; }
-        public int OrderID { get; set; }
-        public float TotalPrice { get; set; }
-        public string DeliveryAddress { get; set; }
+        private Int32 mOrderID;
+        public Int32 OrderID
+        {
+
+            get
+            {
+
+                return mOrderID;
+            }
+            set
+            {
+                mOrderID = value;
+            }
+        }
+        private DateTime mOrderDate;
+        public DateTime OrderDate
+        {
+
+            get
+            {
+                return mOrderDate;
+            }
+            set
+            {
+                mOrderDate = value;
+            }
+        }
+        private Boolean mActive;
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
+
+
+        private string mOrdercontents;
+        public string OrderContents
+        {
+            get
+            {
+                return mOrdercontents;
+            }
+
+            set
+            {
+                mOrdercontents = value;
+            }
+        }
+
+        private float mTotalPrice;
+        public float TotalPrice
+        {
+            get
+            {
+                return mTotalPrice;
+            }
+
+            set
+            {
+                mTotalPrice = value;
+            }
+        }
+
+        private string mDeliveryAddress;
+
+        public string DeliveryAddress
+        {
+            get
+            {
+                return mDeliveryAddress;
+            }
+            set
+            {
+                mDeliveryAddress = value;
+            }
+        }
+
+        public bool Find(int orderID)
+        {
+            mOrderID = 21;
+            mOrderDate = Convert.ToDateTime("16/9/2015");
+            mActive = true;
+            mOrdercontents = " Test content";
+            mTotalPrice = (float)Convert.ToInt32("34.34");
+            mDeliveryAddress = " Test delivery address";
+            return true;
+        }
+
     }
 }
